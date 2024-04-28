3ba1cd2f1647daeaeb47e2984ba31985259e987d
Pauchxk <jamesfvmc@outlook.com>
2024-04-29 00:11:42 +0100

    Added P.Euler problem 1

---
+++ problem1.py
@@ -0,0 +1,9 @@
+#find the sum of all the multiples of 3 or 5 below 1000
+def threeOrFive():
+    total = 0
+    for i in range(1000):
+        if i % 3 == 0 or i % 5 == 0:
+            total += i
+    return total
+
+print(threeOrFive())
