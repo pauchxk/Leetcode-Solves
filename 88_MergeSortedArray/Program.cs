89d04b9f6f24b00be3a365bb2d2702b86a6d8e65
pauchxk <58793330+pauchxk@users.noreply.github.com>
2025-07-27 16:06:23 +0100

    Add files via upload

---
+++ 88_MergeSortedArray/Program.cs
@@ -0,0 +1,32 @@
+﻿public class Solution
+{
+    public void Merge(int[] nums1, int m, int[] nums2, int n)
+    {
+        int p1 = m-1;
+        int p2 = n-1;
+        int i = m+n-1;
+
+        while(p2>=0)
+        {
+            if(p1 >=0 && nums1[p1] > nums2[p2])
+                nums1[i--] = nums1[p1--];
+            else
+                nums1[i--]= nums2[p2--];
+        }
+
+        foreach (var item in nums1) { Console.WriteLine(item); }
+    }
+}
+
+public class Program
+{
+    protected static void Main()
+    {
+        Solution solution = new();
+        int[] nums1 = {1,2,3,0,0,0};
+        int[] nums2 = { 2,5,6 };
+        int m = nums2.Count();
+        int n = nums2.Count();
+        solution.Merge(nums1, m, nums2, n);
+    }
+}
